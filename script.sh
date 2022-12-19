b88658b606fc7b9ec5ce55d95b769d72b37b1516
alfon <agemaliel@gmail.com>
2022-12-20 04:57:39 +0700

    perubahan3

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
 docker build -t agemaliel/from from
+
+docker image ls
+
+#RUN Instruction
+
+docker build -t agemaliel/run run
+
+#CMD
+docker build -t agemaliel/ command command
+
+docker images inspect agemaliel/command
